4354c11e0ae78cf51c5c7c94e0a642333db49c7d
Ferdi Ferial Ahmadi <ferdiahmdi@gmail.com>
2025-03-26 22:44:59 +0700

    feat: client init

---
+++ client/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes></Routes>
+  </BrowserRouter>
+);
